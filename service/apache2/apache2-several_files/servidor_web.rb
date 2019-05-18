6ed8d3d24b0c13eea9a285138d6458c29cd07f24
David <dvarrui@protonmail.com>
2019-05-18 20:08:15 +0100

    U target use (desc, :weight => val)

--- service/apache2/apache2-several_files/servidor_web.rb
+++ service/apache2/apache2-several_files/servidor_web.rb
@@ -1,16 +1,16 @@
 
 group "Servidor Web" do
-  target "Servicio activo"
+  target "Servicio activo", :weight => 5
   goto :host1, :exec => "systemctl status apache2"
-  expect result.find("active (running)").count.equal(1), :weight => 5
+  expect result.find("active (running)").count.equal(1)
 
-  target "Existe fichero index.html"
+  target "Existe fichero index.html", :weight => 5
   goto :host1, :exec => "vdir /srv/www/htdocs"
-  expect result.find("index.html").count.equal(1), :weight => 5
+  expect result.find("index.html").count.equal(1)
 
-  target "Nombre dentro de index.html"
+  target "Nombre dentro de index.html", :weight => 5
   goto :host1, :exec => "cat /srv/www/htdocs/index.html"
-  expect result.find(get(:username)).count.equal(1), :weight => 5
+  expect result.find(get(:username)).count.equal(1)
 
   goto :host1, :exec => "cat /srv/www/htdocs/index.html"
   unique "index_content", result.value
