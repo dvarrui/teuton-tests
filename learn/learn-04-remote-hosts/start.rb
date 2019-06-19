c409e6e5562d634394eb4d00e2464facc3fdfd85
David <dvarrui@protonmail.com>
2019-06-20 00:10:30 +0100

    U

--- learn/learn-04-remote-hosts/start.rb
+++ learn/learn-04-remote-hosts/start.rb
@@ -2,19 +2,19 @@
 group "Configure hostname and DNS server" do
 
   target "Hostname is <#{get(:host1_hostname)}>"
-  goto   :host1, :exec => "hostname -f"
+  goto   :host1, :exec => "hostname"
   expect_one get(:host1_hostname)
 
   target "DNS Server OK"
-  goto   :host1, :exec => "host www.google.es"
-  expect "has address"
+  goto   :host1, :exec => "nslookup www.google.es"
+  expect "Nombre:"
 
 end
 
 group "Create user with your name" do
 
   target "Exist user <#{get(:username)}>"
-  goto   :host1, :exec => "id #{get(:username)}"
+  goto   :host1, :exec => "net user"
   expect get(:username)
 
 end
@@ -22,5 +22,6 @@ end
 play do
   show
   export :format => :colored_text
+  export :format => :json
   send :copy_to => :host1
 end
