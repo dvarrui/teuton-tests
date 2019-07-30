fd23f37576e80602025bdb7802ffef0d21f95165
David <dvarrui@protonmail.com>
2019-07-30 22:06:39 +0100

    U

--- system/basic/command-mkdir/host.rb
+++ system/basic/command-mkdir/host.rb
@@ -1,15 +1,15 @@
 
 group :hostname_configurations do
-  target "Checking SSH port <"+get(:host1_ip)+">"
-  goto  :localhost, :exec => "nmap #{get(:host1_ip)} | grep ssh|wc -l"
-  expect result.eq(1)
+  target "Checking SSH port <#{get(:host1_ip)}>"
+  goto  :localhost, :exec => "nmap #{get(:host1_ip)}"
+  expect result.find("ssh").count.eq(1)
 
   _hostname="#{get(:lastname1)}.#{get(:lastname2)}"
-  target "Checking hostname <"+_hostname+">"
+  target "Checking hostname <#{_hostname}>"
   goto  :host1, :exec => "hostname -f"
   expect result.equal?(_hostname)
 
   unique "hostname", result.value
-  goto  :host1, :exec => "blkid |grep sda1"
+  goto  :host1, :exec => "blkid | grep sda1"
   unique "UUID", result.value
 end
--- system/basic/command-mkdir/user.rb
+++ system/basic/command-mkdir/user.rb
@@ -1,10 +1,10 @@
 
 group :user_definitions do
-  username=get(:firstname)
+  username = get(:firstname)
 
   target "User <#{username}> exists"
-  goto  :host1, :exec => "cat /etc/passwd | grep '#{username}:' | wc -l"
-  expect result.eq(1)
+  goto  :host1, :exec => "cat /etc/passwd"
+  expect result.find(username).count.eq(1)
 
   target "Users <#{username}> with not empty password "
   goto  :host1, :exceute => "cat /etc/shadow | grep '#{username}:' | cut -d : -f 2| wc -l"
