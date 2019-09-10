e4b2882e81923dd6341e2a332239d92fa03f1e93
David <dvarrui@protonmail.com>
2019-09-11 00:16:18 +0100

    U

--- systems.3/02-vnc.dev/debian-configuration.rb
+++ systems.3/02-vnc.dev/debian-configuration.rb
@@ -11,7 +11,6 @@ group "Debian configuration" do
   goto   :debian, :exec => "ifconfig| grep HWaddr| tr -s ' ' '%'| cut -d % -f 5"
   unique "MAC[debian]", result.value
 
-
   target "Set Hostname to " + gett(:debian_hostname)
   goto   :debian, :exec => "hostname -a"
   expect result.equal(get(:debian_hostname))
