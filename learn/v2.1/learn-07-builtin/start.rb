cdd941b72aa6602de1590d37cc35f49d622c1d09
David <dvarrui@protonmail.com>
2019-10-10 14:11:27 +0100

    U builtin demo

--- learn/v2.1/learn-07-builtin/start.rb
+++ learn/v2.1/learn-07-builtin/start.rb
@@ -2,6 +2,12 @@
 group "learn-07-builtin" do
   service('firewalld').is_running?
   service('sshd').is_running?
+
+  user('root').exists?
+  user('david').exists?
+
+  package('geany').installed?
+  package('basic').installed?
 end
 
 play do
