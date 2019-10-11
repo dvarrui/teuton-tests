2aefaa225e370568d5333d81e7b30078f283109e
David <dvarrui@protonmail.com>
2019-10-12 00:47:21 +0100

    U

--- learn/v2.1/learn-09-macros/start.rb
+++ learn/v2.1/learn-09-macros/start.rb
@@ -1,9 +1,9 @@
 use 'user'
 
 group "learn-09-macros" do
-  call 'exist_user', :name => 'root'
-  call 'exist_user', :name => 'david'
-  call 'exist_user', :name => 'fran'
+  call 'exist_user', name: 'root'
+  call 'exist_user', name: 'david'
+  call 'exist_user', name: 'fran'
 end
 
 play do
