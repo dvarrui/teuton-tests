b533d0d841f74ff386e2de06b50ca49bcda5937f
David <dvarrui@protonmail.com>
2019-10-13 08:39:30 +0100

    U

--- learn/v2.1/learn-09-macros/start.rb
+++ learn/v2.1/learn-09-macros/start.rb
@@ -2,8 +2,9 @@ use 'macro/gnulinux'
 
 group "learn-09-macros" do
   call 'exist_user', name: 'root'
-  call 'exist_user', name: 'david'
   call 'exist_user', name: 'fran'
+  
+  call_exist_user(name: 'david')
 end
 
 play do
