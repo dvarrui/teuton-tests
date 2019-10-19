7781e72bf1c77c098a32cc04336f230b63463d01
David <dvarrui@protonmail.com>
2019-10-19 19:08:59 +0100

    U

--- learn/testing/test-windows/cmd.rb
+++ learn/testing/test-windows/cmd.rb
@@ -2,10 +2,17 @@
 group "Windows CMD over SSH" do
 
   target "Run whoami on #{gett(:host1_ip)}"
+  puts "whoami"
   goto   :host1, :exec => "whoami"
   expect get(:host1_username)
 
+  target "Set 4GB of RAM memory for the virtual machine"
+  puts "powershell -command (Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory -eq 4294496256"
+  goto :host1, :exec => "powershell -command (Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory -eq 4294496256"
+  expect "True"
+
   target "Run ps on #{gett(:host1_ip)}"
+  puts "ps"
   goto   :host1, :exec => "ps"
   expect "sshd"
 
