1ab3220d13302dcf73ad8ef0462d0d2d8cacb4bb
David Vargas <dvarrui@protonmail.com>
2022-09-06 13:28:37 +0100

    [U] .

--- en/systems.2/12-debian-backup/opensuse-config.rb
+++ en/systems.2/12-debian-backup/opensuse-config.rb
@@ -1,27 +1,27 @@
 
 group "Configure OpenSUSE 13.2" do
   target "SSH port 22 on #{gett(:opensuse_ip)} open"
-  goto   :localhost, :exec => "nmap #{get(:opensuse_ip)} -Pn"
+  run "nmap #{get(:opensuse_ip)} -Pn"
   expect_one [ 'ssh', 'open']
 
   hostname3a = "#{get(:lastname1)}#{get(:number)}g1"
   target "Checking hostname -a is " + hostname3a
-  goto   :opensuse, :exec => "hostname -a"
+  run "hostname -a", on: :opensuse
   expect result.eq hostname3a
 
   hostname3b="#{get(:domain)}"
   target "Checking hostname -d is "+hostname3b
-  goto   :opensuse, :exec => "hostname -d"
+  run "hostname -d", on: :opensuse
   expect result.eq hostname3b
 
   hostname3c="#{hostname3a}.#{hostname3b}"
   target "Checking hostname -f is " + hostname3c
-  goto   :opensuse, :exec => "hostname -f"
+  run "hostname -f", on: :opensuse
   expect result.eq hostname3c
 
-  goto   :opensuse, :exec => "blkid |grep sda1"
+  run "blkid |grep sda1", on: :opensuse
   unique "UUID_sda1", result.value
-  goto   :opensuse, :exec => "blkid |grep sda2"
+  run "blkid |grep sda2", on: :opensuse
   unique "UUID_sda2", result.value
 end
 
@@ -29,14 +29,14 @@ group "Configure users" do
   username = get(:firstname)
 
   target "User #{username} exists"
-  goto   :opensuse, :exec => "id #{username}"
+  run "id #{username}", on: :opensuse
   expect_one username
 
   target "User #{username} with not empty password "
-  goto   :opensuse, :exec => "cat /etc/shadow | grep '#{username}:' | cut -d : -f 2| wc -l"
+  run "cat /etc/shadow | grep '#{username}:' | cut -d : -f 2| wc -l", on: :opensuse
   expect result.equal(1)
 
   target "User #{username} logged"
-  goto   :opensuse, :exec => "last"
+  run "last", on: :opensuse
   expect username[0,8]
 end
--- en/systems.2/12-debian-backup/windows-config.rb
+++ en/systems.2/12-debian-backup/windows-config.rb
@@ -1,15 +1,15 @@
 
 group "Configure W7 and W2008server" do
   target "ping #{gett(:win7_ip)}"
-  goto   :localhost, :exec => "ping #{get(:win7_ip)} -c 1|"
+  run "ping #{get(:win7_ip)} -c 1|"
   expect_none 'Destination Host Unreachable'
 
   target "ping #{gett(:ws2008_ip)}"
-  goto   :localhost, :exec => "ping #{get(:ws2008_ip)} -c 1"
+  run "ping #{get(:ws2008_ip)} -c 1"
   expect_none 'Destination Host Unreachable'
 
   target "Netbios-SSN service on #{gett(:ws2008_ip)}"
-  goto   :localhost, :exec => "nmap -Pn #{get(:ws2008_ip)}"
+  run "nmap -Pn #{get(:ws2008_ip)}"
   expect_one ['139/tcp', 'open']
 
   target "Microsoft-DS service on #{gett(:ws2008_ip)}"
