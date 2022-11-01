673972baa007378c7626584470af09c73f78e4cd
dvarrui <dvarrui@protonmail.com>
2022-11-01 19:59:30 +0000

    [U] .

--- es/sistemas.3/scripting/usermin/vagrant/install-software.sh
+++ es/sistemas.3/scripting/usermin/vagrant/install-software.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-DISTRO=debian
+DISTRO=USERMINE
+AUTHOR=teuton-software
+URL=https://github.com/teuton-software/teuton-tests
 
 apt update
 apt install -y vim nano
 apt install -y tree nmap
 apt install -y git
+apt install -y ruby 
 
 apt install -y figlet
 figlet $DISTRO > /etc/motd
 echo "" >> /etc/motd
-echo "David Vargas Ruiz" >> /etc/motd
-echo "https://github.com/dvarrui" >> /etc/motd
+echo $AUTHOR >> /etc/motd
+echo $URL >> /etc/motd
 
 echo "# Adding more alias" >> /home/vagrant/.bashrc
 echo "alias c='clear'" >> /home/vagrant/.bashrc
