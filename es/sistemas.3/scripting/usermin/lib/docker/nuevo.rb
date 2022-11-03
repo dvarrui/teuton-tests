1a91997d4fe4b753b9474e90276d63ff9d09598a
David Vargas <dvarrui@protonmail.com>
2022-11-03 15:17:33 +0000

    [U] .

--- es/sistemas.3/scripting/usermin/lib/docker/nuevo.rb
+++ es/sistemas.3/scripting/usermin/lib/docker/nuevo.rb
@@ -1,15 +1,18 @@
 group "Opción --nuevo USERNAME" do
   cmd = get(:cmd).join(" ")
 
-  users = [ "vader1", "obiwan2", "yoda3" ]
-  peso = 20.0/users.size
+  peso = 20
+  name = "demo"
+  target "script --nuevo #{name}", weight: peso
+  run "#{cmd} --nuevo #{name} teuton /bin/bash 123456; clear; id #{name}", on: :host1
+  expect_one "(#{name}) "
 
-  users.each do |name|
-    target "script --nuevo #{name} teuton /bin/bash 123456", weight: peso
+  #users = [ "vader1", "obiwan2", "yoda3" ]
+  #peso = 20.0/users.size
 
-    run "#{cmd} --nuevo #{name}", on: :host1
-    run "id #{name}", on: :host1
-    result.restore
-    expect_one "(#{name}) "
-  end
+  #users.each do |name|
+  #  target "script --nuevo #{name} teuton /bin/bash 123456", weight: peso
+  #  run "#{cmd} --nuevo #{name}; clear; id #{name}", on: :host1
+  #  expect_one "(#{name}) "
+  #end
 end
