2d4e4fcfb293b86ce957c14eb2cdc280a08084b3
David Vargas <dvarrui@protonmail.com>
2022-11-09 13:02:19 +0000

    [U] .

--- en/systems.2/13-win-dual-installation/start.rb
+++ en/systems.2/13-win-dual-installation/start.rb
@@ -1,25 +1,10 @@
 
-use 'opensuse'
-use 'linux'
-use 'disk_and_partitions'
+use 'lib/opensuse'
+use 'lib/linux'
+use 'lib/disk_and_partitions'
 
 start do
 	show
-	export :format => :colored_text
-  send :copy_to => :host1
+	export format: :colored_text
+  send copy_to: :host1
 end
-
-=begin
----
-:global:
-  :opensuse1_username: root
-  :dominio: curso1819
-:cases:
-- :tt_members: VALUE
-  :number: '99'
-  :opensuse1_ip: 172.18.7.31
-  :opensuse1_password: 'secret'
-  :firstname: VALUE
-  :lastname1: VALUE
-  :lastname2: VALUE
-=end
