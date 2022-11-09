b0f08bb43c79a5d58af02fead35dde3b81980af2
David Vargas <dvarrui@protonmail.com>
2022-11-09 13:07:34 +0000

    [U] .

--- en/systems.2/12-debian-backup/start.rb
+++ en/systems.2/12-debian-backup/start.rb
@@ -17,18 +17,3 @@ start do
 	show
 	export
 end
-
-=begin
----
-:global:
-  :host1_username: root
-:cases:
-- :tt_members: david
-  :host1_ip: 172.19.2.11
-  :host2_ip: 172.19.2.21
-  :host3_ip: 172.19.2.51
-  :host1_password: 45454545a
-  :firstname: david
-  :lastname1: vargas
-  :lastname2: ruiz
-=end
