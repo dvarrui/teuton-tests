de9fcb3fb4810be7c75788736ca36d3e2fbe2ffd
David Vargas <dvarrui@protonmail.com>
2022-12-16 09:54:48 +0000

    [U] .

--- es/servicios/squid-proxy-cache/start.rb
+++ es/servicios/squid-proxy-cache/start.rb
@@ -1,10 +1,9 @@
 
-require_relative 'host-config'
-require_relative 'squid-service'
+use 'host-config'
+use 'squid-service'
 
 # play ONLY on localhost
-
 play do
   show
-  export :format => :colored_text
+  export format: :colored_text
 end
