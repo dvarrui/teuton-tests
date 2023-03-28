b660740a3996113207055e0b618485270740984c
David Vargas <dvarrui@protonmail.com>
2023-03-28 08:43:05 +0100

    U

--- en/systems.2/32-debian-nagios/debian1-ping.rb
+++ en/systems.2/32-debian-nagios/debian1-ping.rb
@@ -1,22 +1,20 @@
-# encoding: utf-8
 
-task "Ping from debian1 to *" do  
-   
+group "Ping from debian1 to *" do
+
   target "ping debian1 to debian2_ip"
-  goto :debian1, :exec => "ping #{get(:debian2_ip)} -c 1"
-  expect result.grep!("64 bytes from").count!.eq 1
+  run "ping #{get(:debian2_ip)} -c 1", on: :debian1
+  expect_one "64 bytes from"
 
   target "ping debian1 to debian2_name"
-  goto :debian1, :exec => "ping #{@short_hostname[2]} -c 1"
-  expect result.grep!("64 bytes from").count!.eq 1
+  run "ping #{@short_hostname[2]} -c 1", on: :debian1
+  expect_one "64 bytes from"
 
   target "ping debian1 to windows1_ip"
-  goto :debian1, :exec => "ping #{get(:windows1_ip)} -c 1"
-  expect result.grep!("64 bytes from").count!.eq 1
+  run "ping #{get(:windows1_ip)} -c 1", on: :debian1
+  expect_one "64 bytes from"
 
   target "ping debian1 to windows1_name"
-  goto :debian1, :exec => "ping #{@short_hostname[3]} -c 1"
-  expect result.grep!("64 bytes from").count!.eq 1
+  run "ping #{@short_hostname[3]} -c 1", on: :debian1
+  expect_one "64 bytes from"
 
 end
-
