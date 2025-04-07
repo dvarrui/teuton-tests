669a97832b9e47aee58d0212cac0adc829d5c869
David Vargas <dvarrui@protonmail.com>
2025-04-07 19:46:58 +0100

    [U]

--- es/gns3/lab01/test3/interfaces.rb
+++ es/gns3/lab01/test3/interfaces.rb
@@ -5,12 +5,13 @@ group "Configurar las interfaces de red" do
 
   target "Configurar pc1 ip <#{get(:pc1_ip)}>"
   set(:gns3server_port, get(:pc1_console))
-  run "show ip", on: :gns3server
+  run "show ip", on: :gns3server, encoding: 'ASCII-8BIT'
+  result.debug
   expect get(:pc1_ip)
  
   target "Configurar pc2 ip <#{get(:pc2_ip)}>"
   set(:gns3server_port, get(:pc2_console))
-  run "show ip", on: :gns3server
+  run "show ip", on: :gns3server, encoding: 'ASCII-8BIT'
   expect get(:pc2_ip)
 
 end
@@ -22,12 +23,12 @@ group "Comprobar la conectividad entre los dispositivos" do
 
   target "ping pc1 -> pc2"
   set(:gns3server_port, get(:pc1_console))
-  run "ping #{get(:pc2_ip)} -c 4", on: :gns3server
+  run "ping #{get(:pc2_ip)} -c 4", on: :gns3server, encoding: 'ASCII-8BIT'
   expect "bytes from"
 
   target "ping pc2 -> pc1"
   set(:gns3server_port, get(:pc2_console))
-  run "ping #{get(:pc1_ip)} -c 4", on: :gns3server
+  run "ping #{get(:pc1_ip)} -c 4", on: :gns3server, encoding: 'ASCII-8BIT'
   expect "bytes from"
 
 end
